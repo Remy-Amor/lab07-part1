bbb4a00cb195090254ae7a3cd4174c545678d64f
Remy-Amor <105914423@student.swin.edu.au>
2025-04-26 11:19:49 +1000

    created daysarray

---
+++ daysarray.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="author" content="Remy Amor">
+     <meta name="description" content="working with arrays php">
+     <meta name="keywords" content="php, variables, functions, arrays">
+     <title>Days Array</title>
+</head>
+<body>
+     
+</body>
+</html>
